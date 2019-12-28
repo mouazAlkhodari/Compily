059e9668051e7208b212258883dbad5b9f1d4cfc
Mhd Sami AlEsh <sami-esh@hotmail.com>
2019-12-28 18:36:47 +0200

    Fix for node with data types

--- src/nodes/ForNode.java
+++ src/nodes/ForNode.java
@@ -20,24 +20,54 @@ public class ForNode extends AbstractTreeNode {
 
 	@Override
 	public Object execute(Context context) {
-		double start = (Double)children.get(0).execute(context);
-		double end = (Double)children.get(1).execute(context);
-		double step = 1;
-		if (children.size() > 3)
-			step = (Double)children.get(3).execute(context);
-		double current = start;
-		while(true){
-			if (step < 0 && current < end)
-				break;
-			if (step > 0 && current > end)
-				break;
-			context.getVars().put(varName, current);
-			children.get(2).execute(context);
-			end = (Double)children.get(1).execute(context);
+
+		Object val1 = children.get(0).execute(context);
+		Object val2 = children.get(1).execute(context);
+
+		if(val1 instanceof Integer && val2 instanceof Integer){
+			int start = (int)val1;
+			int end = (int)val2;
+			int step = 1;
+			if (children.size() > 3)
+				step = (int)children.get(3).execute(context);
+			double current = start;
+			while(true){
+				if (step < 0 && current < end)
+					break;
+				if (step > 0 && current > end)
+					break;
+				context.getVars().put(varName, current);
+				children.get(2).execute(context);
+				end = (int)children.get(1).execute(context);
+				if (children.size() > 3)
+					step = (int)children.get(3).execute(context);
+				current += step;
+			}
+		}
+		else if(val1 instanceof Double && val2 instanceof Double){
+			double start = (Double)val1;
+			double end = (Double)val2;
+			double step = 1;
 			if (children.size() > 3)
 				step = (Double)children.get(3).execute(context);
-			current += step;
+			double current = start;
+			while(true){
+				if (step < 0 && current < end)
+					break;
+				if (step > 0 && current > end)
+					break;
+				context.getVars().put(varName, current);
+				children.get(2).execute(context);
+				end = (Double)children.get(1).execute(context);
+				if (children.size() > 3)
+					step = (Double)children.get(3).execute(context);
+				current += step;
+			}
 		}
+		else{
+			throw new RuntimeException("Invalid For iterator type (should be int/float) man !!!");
+		}
+
 		return null;
 	}
 
